362b5148421e87d131e1a6d668efe7775076c635
Kyle <kcparks1234@gmail.com>
2023-10-30 18:44:53 -0700

    Added the navlink back

--- DrawerNavigation.js
+++ DrawerNavigation.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from './home';
 
-
 const Drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
   return (
-    <Drawer.Navigator initialRouteName="Home">
-      <Drawer.Screen name="Home" component={Home} options={{ title: '' }} />
-
+    <Drawer.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        drawerActiveTintColor: 'black',  // Color for the active link
+        drawerInactiveTintColor: 'gray', // Color for the inactive link
+        drawerActiveBackgroundColor: 'white',  // Background color for the active link
+        drawerStyle: { marginVertical: 5 },
+      }}
+    >
+      <Drawer.Screen name="Home" component={Home} options={{ title: 'Home' }} />
     </Drawer.Navigator>
   );
 }
